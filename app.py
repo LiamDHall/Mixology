cf3e1a02ae917cfc4e6e0059e422ce9d3cbcff39
LiamDHall <hall.d.liam@gmail.com>
2020-12-30 22:02:29 +0000

    Connect flask to mongo db and update app dependencies

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def test():
-    return "hello test"
+@app.route("/home")
+def home():
+    cocktails = mongo.db.cocktails.find()
+    return render_template("home.html", cocktails=cocktails)
 
 
 if __name__ == "__main__":
