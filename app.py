732a149fd93f0cfb9f4de71a739f6642b6c30e41
LiamDHall <hall.d.liam@gmail.com>
2021-01-10 22:29:42 +0000

    Link site header navbar to template pages that renders based on the link
    The url of the page is produced for the link clicked the navbar.
    The page h1 of the page is also rendered to match that of the url.
    Currently the record that is indexed when the link is clicked is
    rendered underneath the hero image this is to be removed before the
    deployment.

--- app.py
+++ app.py
@@ -16,11 +16,26 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+# Set alcohol categories as a global variable so all templates can call it
+@app.context_processor
+def get_category():
+    alcohol_categories = list(mongo.db.alcohol.find())
+    return dict(alcohol_categories=alcohol_categories)
+
+
 @app.route("/")
 @app.route("/home")
 def home():
     cocktails = list(mongo.db.cocktails.find())
     return render_template("home.html", cocktails=cocktails)
+
+
+@app.route("/<alcohol_name>", methods=["GET", "POST"])
+def category(alcohol_name):
+    alcohol = mongo.db.alcohol.find_one({"alcohol_name": alcohol_name})
+    cocktails = list(mongo.db.cocktails.find())
+    return render_template(
+        "category.html", alcohol=alcohol, cocktails=cocktails)
 
 
 if __name__ == "__main__":
