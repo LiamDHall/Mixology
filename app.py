e2219283f4db1bb8a9d4d7c9c6fcb0850f9aad21
LiamDHall <hall.d.liam@gmail.com>
2021-02-10 21:53:10 +0000

    Test 404 error handler in live site as gitpod returns home in preview

--- app.py
+++ app.py
@@ -654,6 +654,13 @@ def submit_rating(user_rated_cocktails):
             mongo.db.users.update_one(user_query, user_update)
 
 
+# Error Handler 404 Page Not Found
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
