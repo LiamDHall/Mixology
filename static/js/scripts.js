799e727a5500febb403e69c75a0b68de83ba7170
LiamDHall <hall.d.liam@gmail.com>
2021-02-13 17:05:43 +0000

    scripts: Make rec card carousel responsive to screen size

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,23 +1,95 @@
-//  Swiper JS
-var mySwiper = new Swiper('.swiper-container', {
-    // Optional Parameters
-    slidesPerView: 6,
-    spaceBetween: 10,
-    slidesPerGroup: 6,
-    direction: 'horizontal',
-    loop: false,
+//Set inital value for Swiper JS
+$(document).ready(function() {
+    var width = $(window).width();
+    if (width > 1440) {
+        noSlides = 6;
+    }
 
-    //  Navigation Arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+    else if (width > 768) {
+        noSlides = 5
+    }
 
-    //  Pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-})
+    else if (width > 425) {
+        noSlides = 4
+    }
+
+    else {
+        noSlides = 3;
+    }
+
+    console.log(width)
+    console.log(noSlides)
+
+    //  Swiper JS
+    var mySwiper = new Swiper('.swiper-container', {
+
+        // Optional Parameters
+        slidesPerView: noSlides,
+        slidesPerGroup: noSlides,
+        
+        spaceBetween: 10,
+        direction: 'horizontal',
+        loop: false,
+
+        //  Navigation Arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        //  Pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+});
+
+//Set dynamic value for Swiper JS
+var noSlides = 3;
+window.addEventListener('resize', function(){
+    var width = $(window).width();
+    if (width > 1440) {
+        noSlides = 6;
+    }
+
+    else if (width > 768) {
+        noSlides = 5
+    }
+
+    else if (width > 425) {
+        noSlides = 4
+    }
+
+    else {
+        noSlides = 3;
+    }
+
+    console.log(width)
+    console.log(noSlides)
+
+    //  Swiper JS
+    var mySwiper = new Swiper('.swiper-container', {
+
+        // Optional Parameters
+        slidesPerView: noSlides,
+        slidesPerGroup: noSlides,
+        
+        spaceBetween: 10,
+        direction: 'horizontal',
+        loop: false,
+
+        //  Navigation Arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        //  Pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+});
 
 /* Image Preview
 Source https://stackoverflow.com/questions/20820705/render-preview-image-from-url-input
@@ -28,7 +100,7 @@ $('[name="cocktail-img-url"]').on('change', function() {
 // (END OF NOT ALL MY OWN CODE)
 
 //  Navbar Active Link Toggler
-$( document ).ready(function() {
+$(document).ready(function() {
     var currentPage = window.location.href;                                 //  Get current page url
     var pageTitle = currentPage.substr(currentPage.lastIndexOf('/') + 1);   //  Get page title after domain "/"
     $('.nav-item').removeClass('active');                                   //  Remove active class off old nav link
