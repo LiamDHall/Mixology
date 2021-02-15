9a1da27b0169ec48537a520a7cec14ab41b0863e
LiamDHall <hall.d.liam@gmail.com>
2021-02-15 04:58:22 +0000

    form validation handler: Remove console log

--- static/js/form-validation-handler.js
+++ static/js/form-validation-handler.js
@@ -8,8 +8,6 @@ Starter JavaScript for disabling form submissions if there are invalid fields
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
   var forms = document.querySelectorAll('.needs-validation');
-    console.log('I ran');
-    console.log('forms');
   // Loop over them and prevent submission
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
